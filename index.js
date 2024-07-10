23657daa95021a4397affbce4bd9724bc70abfd7
vaishnavishahakar <vaishnavishahakar1@gmail.com>
2024-07-10 22:25:27 +0530

    Program to say Hello to all elements from array using key of loop

---
+++ index.js
@@ -0,0 +1,14 @@
+const employees = [
+    "Priya",
+    "Snehal",
+    "Rishi",
+    "Yamini",
+    "Sanket",
+    "Krishna",
+    "kush"
+]
+
+for(const employ of employees)
+{
+    console.log(`Hello ${employ}`)
+}
