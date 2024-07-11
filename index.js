9d58e69ed8830db4ffe8e2d48dbd1f10e4b3b07f
vaishnavishahakar <vaishnavishahakar1@gmail.com>
2024-07-11 18:33:33 +0530

    Program to find the sum of odd numbers from the given array

--- index.js
+++ index.js
@@ -15,12 +15,23 @@
 // }
 
 // Program to find the largest number from the given array
-const marks = [99, 500, 100, 70]
-let largest = marks[0]
-for(const num of marks)
+// const marks = [99, 500, 100, 70]
+// let largest = marks[0]
+// for(const num of marks)
+// {
+//     if(num > largest){
+//         largest = num
+//     }
+// }
+// console.log(largest)
+
+// Program to find the sum of odd numbers from the given array
+const numbers = [7, 4, 12, 20, 7, 15]
+let sum = 0;
+for(let i=0; i<numbers.length; i++)
 {
-    if(num > largest){
-        largest = num
+    if(numbers[i]%2==1){
+        sum+= numbers[i]
     }
 }
-console.log(largest)
+console.log(`Sum: ${sum}`)
