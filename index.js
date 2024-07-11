cc33153b39042b9c52fd1817023b05ac88408874
vaishnavishahakar <vaishnavishahakar1@gmail.com>
2024-07-11 18:23:32 +0530

    Program to find the largest number from the given array

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const employees = [
-    "Priya",
-    "Snehal",
-    "Rishi",
-    "Yamini",
-    "Sanket",
-    "Krishna",
-    "kush"
-]
+// Program to say Hello to all elements from any array use key of loop
+// const employees = [
+//     "Priya",
+//     "Snehal",
+//     "Rishi",
+//     "Yamini",
+//     "Sanket",
+//     "Krishna",
+//     "kush"
+// ]
 
-for(const employ of employees)
+// for(const employ of employees)
+// {
+//     console.log(`Hello ${employ}`)
+// }
+
+// Program to find the largest number from the given array
+const marks = [99, 500, 100, 70]
+let largest = marks[0]
+for(const num of marks)
 {
-    console.log(`Hello ${employ}`)
+    if(num > largest){
+        largest = num
+    }
 }
+console.log(largest)
